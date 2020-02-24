39a5c02c3c6f367a35ba2b4ef6e6d4cfa9302c61
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-02-24 14:30:06 -0400

    conclusao envio de email

---
+++ README.md
@@ -0,0 +1,1 @@
+Aplicação desenvolvida até o momento em nodeJS, uma aplicação para uma barbearia comercial.
---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'd7e75878702f07',
+    pass: '9b4ae7a74551f5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
