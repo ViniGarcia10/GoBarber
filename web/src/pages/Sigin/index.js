130bbc8afad0b14b31301583cca8c339236eda5e
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-17 16:54:42 -0400

    config styled pages

--- web/src/pages/Sigin/index.js
+++ web/src/pages/Sigin/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Logo from '~/assets/images/logo.svg';
 
 export default function SingIn() {
-  return <div>SingIn</div>;
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register"> Criar conta gratuita </Link>
+      </form>
+    </>
+  );
 }
--- web/src/pages/SingUp/index.js
+++ web/src/pages/SingUp/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function SingUp() {
-  return <div>SingUp</div>;
+import Logo from '~/assets/images/logo.svg';
+
+export default function SingIn() {
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/"> Já tenho Login </Link>
+      </form>
+    </>
+  );
 }
