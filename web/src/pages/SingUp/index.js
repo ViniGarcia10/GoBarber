9703634028635c8c126da62719d6e5dbc78a81c9
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-18 06:39:18 -0400

    implementing yup

--- web/src/pages/SingUp/index.js
+++ web/src/pages/SingUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import Logo from '~/assets/images/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string()
+    .min(6, 'Senha com no mínimo 6 caracteres!')
+    .required('Insira seu nome Completo!'),
+  email: Yup.string()
+    .email('Insira um e-mail válido!')
+    .required('O e-mail é Obrigatório!'),
+  password: Yup.string().required('A senha é obrigatória!'),
+});
 
 export default function SingUp() {
   function handleSubmit(data) {
@@ -13,7 +24,7 @@ export default function SingUp() {
     <>
       <img src={Logo} alt="GoBarber" />
 
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
