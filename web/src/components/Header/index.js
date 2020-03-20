fe9a163513306de6fc9f8c082730c27480aa1dfc
ViniGarcia10 <viniciusgarcia49@gmail.com>
2020-03-20 19:58:19 -0400

    config layout header

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logoPurple from '~/assets/images/logoPurple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logoPurple} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Vinícius Garcia</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Vinícius Garcia Leão"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
